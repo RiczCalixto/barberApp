fef82cf0c1911b05738c4f505fdb873e6415b456
Ricardo Calixto <rcalixtos@gmail.com>
2019-07-28 15:58:48 -0300

    Introdução do middleware global de autenticação de token (usado na rota de update de usuario).

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -12,6 +12,32 @@ class UserController {
 
     return res.json({ id, name, email, provider });
   }
+
+  async update(req, res) {
+    const { email, oldPassword } = req.body;
+    const user = await User.findByPk(req.userId);
+    if (email !== user.email) {
+      const userExists = await User.findOne({
+        where: { email },
+      });
+
+      if (userExists) {
+        return res.status(401).json({ error: 'user already exists' });
+      }
+    }
+
+    if (oldPassword === req.body.password) {
+      return res.status(401).json({ error: 'NEW PW MUST BE != FROM OLD' });
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'pw does not match' });
+    }
+
+    const { id, name, provider } = await user.update(req.body);
+
+    return res.json({ id, name, email, provider });
+  }
 }
 
 export default new UserController();
--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,15 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/session', SessionController.store);
+// Fez um middleware global que serve para as rotas apos o use
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
 
 export default routes;
