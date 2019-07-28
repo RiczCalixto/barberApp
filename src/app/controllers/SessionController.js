ee1efc619b57ae6bfdb61de46bdc2cb167f7989f
Ricardo Calixto <rcalixtos@gmail.com>
2019-07-28 15:17:13 -0300

    Importação do jwt para autenticação de sessão a partir de pw e senha provido pelo usuário. A string e o tempo de expiração do token foram isolados no arquivo de auth.js que esta dentro da pasta de config. Foi criado um metodo no arquivo de UserController para verificar se o pw está correto. Criado arquivo SessionController (que usa o metodo checkPassword) para verificar se email e senha batem com as informaçoes do banco e, caso ocorra tudo certo, gera um token que sera usado no futuro para dizer que o usuario esta autenticado.

---
+++ src/app/controllers/SessionController.js
@@ -0,0 +1,34 @@
+import jwt from 'jsonwebtoken';
+import authConfig from '../../config/auth';
+import User from '../models/Users';
+
+class SessionController {
+  async store(req, res) {
+    const { email, password } = req.body;
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      res.status(401).json({ error: 'user not found' });
+    }
+
+    if (!(await user.checkPassword(password))) {
+      return res.status(401).json({ error: 'user not found2' });
+    }
+
+    const { id, name } = user;
+    return res.json({
+      user: { id, name, email },
+      token: jwt.sign({ id }, authConfig.secret, {
+        expiresIn: authConfig.expiresIn,
+      }),
+    });
+
+    /* obs: O primeitro parametro do jwt.sign({}) é o payload, o segundo
+    parametro do é uma string. No caso foi gerado no site md5 digitando
+    RocketSeatGoBarber. O terceiro parâmetro é o tempo de expiração do Token
+    O segundo e terceiro parametro estao no arquivo authConfig que esta dentro
+    da pagina config  */
+  }
+}
+
+export default new SessionController();
---
+++ src/config/auth.js
@@ -0,0 +1,6 @@
+// Contém configurações da parte de autenticação da nossa aplicação
+
+export default {
+  secret: 'ad7a807dcac8ed9cb673a949e461076d',
+  expiresIn: '7d',
+};
