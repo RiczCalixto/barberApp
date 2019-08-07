ef74b5af754224e2a9bce52a204003bf2b5c972c
Ricardo Calixto <rcalixtos@gmail.com>
2019-08-06 23:30:48 -0300

    listagem de agendamento do usuario, paginaçãoda listagem de agendamento, listagem de agendamento para providers (rota e controle de schedule).

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,9 +1,37 @@
 import * as Yup from 'yup';
 import { startOfHour, parseISO, isBefore } from 'date-fns';
 import User from '../models/Users';
+import File from '../models/File';
 import Appointment from '../models/Appointment';
 
 class AppointmentController {
+  async index(req, res) {
+    const { page = 1 } = req.query;
+    const appointments = await Appointment.findAll({
+      where: { user_id: req.userId, canceled_at: null },
+      attributes: ['id', 'date'],
+      order: ['date'],
+      limit: 20,
+      offset: (page - 1) * 20,
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['id', 'name'],
+          include: [
+            {
+              model: File,
+              as: 'avatar',
+              attributes: ['id', 'path', 'url'],
+            },
+          ],
+        },
+      ],
+    });
+
+    return res.json(appointments);
+  }
+
   async store(req, res) {
     const schema = Yup.object().shape({
       provider_id: Yup.number().required(),
@@ -30,7 +58,6 @@ class AppointmentController {
     // check for past dates
 
     const hourStart = startOfHour(parseISO(date));
-    console.log(hourStart);
 
     if (isBefore(hourStart, new Date())) {
       return res.status(400).json({ error: 'past dates are not permitted' });
---
+++ src/app/controllers/ScheduleController.js
@@ -0,0 +1,35 @@
+import { startOfDay, parseISO, endOfDay } from 'date-fns';
+import { Op } from 'sequelize';
+import User from '../models/Users';
+import Appointment from '../models/Appointment';
+
+class ScheduleController {
+  async index(req, res) {
+    const checkIfUser = await User.findOne({
+      where: { id: req.userId, provider: true },
+    });
+    if (!checkIfUser) {
+      return res.status(400).json({
+        error: 'Sorry, but you must be a provider to access schedules',
+      });
+    }
+
+    const { date } = req.query;
+    const parseDate = parseISO(date);
+
+    const appointments = await Appointment.findAll({
+      where: {
+        provider_id: req.userId,
+        canceled_at: null,
+        date: {
+          [Op.between]: [startOfDay(parseDate), endOfDay(parseDate)],
+        },
+      },
+      order: ['date'],
+    });
+
+    return res.json(appointments);
+  }
+}
+
+export default new ScheduleController();
